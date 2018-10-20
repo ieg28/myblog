38a7b006330b26591a2a4676ac1be35242a575e3
Ibrahim Ethem Gursoy <iegursoy.2888@gmail.com>
2018-10-20 12:43:10 +0300

    add splash

---
+++ app/src/main/java/com/ieg/myblog/Consts.java
@@ -0,0 +1,5 @@
+package com.ieg.myblog;
+
+public class Consts {
+    public static long SPLASH_MILLIS = 3000;
+}
---
+++ app/src/main/java/com/ieg/myblog/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.ieg.myblog;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, Consts.SPLASH_MILLIS);
+    }
+}
